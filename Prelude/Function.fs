b3685ea48e21c9e4545722fbbf8a58197b1ec425
Kenneth Watkins <vercore5866@gmail.com>
2022-01-16 10:41:17 -0500

    Added Function

---
+++ Prelude/Function.fs
@@ -0,0 +1,19 @@
+﻿namespace Prelude
+
+open System.Collections.Generic
+
+[<AutoOpen>]
+module Function =
+    (*  -- Memoization function - http://www.fssnip.net/63/title/memoizeBy
+
+        -- A cached version of Type.GetGenericArguments()
+            let cachedGetGenericArguments : Type -> Type[] =
+                memoizeBy (fun t -> t.FullName) (fun t -> t.GetGenericArguments()) *)
+    let memoizeBy inputToKey f =
+        let cache = Dictionary<_, _>()
+        fun x ->
+            let k = inputToKey x
+            if cache.ContainsKey(k) then cache.[k]
+            else let res = f x
+                 cache.[k] <- res
+                 res

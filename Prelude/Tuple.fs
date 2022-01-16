846ceb19ecfb847e878262931c48adb865321635
Kenneth Watkins <vercore5866@gmail.com>
2022-01-16 10:32:45 -0500

    Added Tuple.fs

---
+++ Prelude/Tuple.fs
@@ -0,0 +1,32 @@
+﻿namespace Prelude
+
+module Tuple =
+    let uncurry (t1, t2) = K t1 t2
+    let uncurry_3 (t1, t2, t3) = K t1 t2 t3
+    let uncurry_4 (t1, t2, t3, t4) = K t1 t2 t3 t4
+    let uncurry_5 (t1, t2, t3, t4, t5) = K t1 t2 t3 t4 t5
+    let uncurry_6 (t1, t2, t3, t4, t5, t6) =
+        K t1 t2 t3 t4 t5 t6
+    let uncurry_7 (t1, t2, t3, t4, t5, t6, t7) =
+        K t1 t2 t3 t4 t5 t6 t7
+    let uncurry_8 (t1, t2, t3, t4, t5, t6, t7, t8) =
+        K t1 t2 t3 t4 t5 t6 t7 t8
+    let uncurry_9 (t1, t2, t3, t4, t5, t6, t7, t8, t9) =
+        K t1 t2 t3 t4 t5 t6 t7 t8 t9
+    let uncurry_10 (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10) =
+        K t1 t2 t3 t4 t5 t6 t7 t8 t9 t10
+
+    let curry t1 t2 = (t1,t2)
+    let curry_3 t1 t2 t3 = (t1,t2,t3)
+    let curry_4 t1 t2 t3 t4 = (t1,t2,t3,t4)
+    let curry_5 t1 t2 t3 t4 t5 = (t1,t2,t3,t4,t5)
+    let curry_6 t1 t2 t3 t4 t5 t6 =
+         (t1,t2,t3,t4,t5,t6)
+    let curry_7 t1 t2 t3 t4 t5 t6 t7 =
+         (t1,t2,t3,t4,t5,t6,t7)
+    let curry_8 t1 t2 t3 t4 t5 t6 t7 t8 =
+         (t1,t2,t3,t4,t5,t6,t7,t8)
+    let curry_9 t1 t2 t3 t4 t5 t6 t7 t8 t9 =
+         (t1,t2,t3,t4,t5,t6,t7,t8,t9)
+    let curry_10 t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 =
+         (t1,t2,t3,t4,t5,t6,t7,t8,t9,t10)
